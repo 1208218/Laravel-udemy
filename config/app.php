f22f940d599aaf7ca0cd45b1814bf557c34f3025
1208218 <1208218@rgu.ac.uk>
2018-01-25 01:05:22 +0000

    a

--- config/app.php
+++ config/app.php
@@ -163,6 +163,8 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
+        'Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider', // Laravel IDE helper
+
         /*
          * Package Service Providers...
          */
